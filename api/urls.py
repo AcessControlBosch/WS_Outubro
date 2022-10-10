e328d58745af91675eac2aeaf8a77e2f5f2b74b5
AcessControlBosch <silveiradanyelly@gmail.com>
2022-10-10 11:02:28 -0300

    patch no machine

---
+++ api/urls.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path("associates/", AssociateAPI.as_view(), name="associates"),
+    path("associates/<int:pk>/", AssociateAPI.as_view(), name="associatesParameters"),
+
+    path("machines/", MachineAPI.as_view(), name="machines"),
+    path("machines/<int:pk>/", MachineAPI.as_view(), name="machinesParameters"),
+
+    path("questions/", QuestionAPI.as_view(), name="questions"),
+    path("questions/<int:pk>/", QuestionAPI.as_view(), name="questionsParameters"),
+
+    path("greenbooks/", GreenBookAPI.as_view(), name="greenbooks"),
+    path("greenbooks/<int:pk>/<int:tq>/", GreenBookAPI.as_view(), name="greenbooksParameters"),
+
+    path("maintenances/", MaintenanceAPI.as_view(), name="maintenances"),
+    path("maintenances/<int:pk>/", MaintenanceAPI.as_view(), name="maintenancesParameters"),
+
+    path("releasemachines/", ReleaseMachineAPI.as_view(), name="releasemachines"),
+    path("releasemachines/<int:pk>/", ReleaseMachineAPI.as_view(), name="releasemachinesParameters"),
+
+    path("observations/", ObservationAPI.as_view(), name="observations"),
+    path("observations/<int:pk>/", ObservationAPI.as_view(), name="observationsParameters"),
+
+    path("areas/", AreaAPI.as_view(), name="areas"),
+    path("areas/<int:pk>/", AreaAPI.as_view(), name="areasParameters"),
+
+    # path("requestLogins/", RequestLoginAPI.as_view(), name="requestLogins"),
+    # path("requestLogins/<int:pk>/", RequestLoginAPI.as_view(), name="requestLoginsParameters"),
+
+]
